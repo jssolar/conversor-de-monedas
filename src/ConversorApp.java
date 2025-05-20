bcd17d1ffc488e0f5f7b6ec36261b3e92566173b
Juan Solar <juansolarsolar@yahoo.com>
2025-05-19 23:25:25 -0400

    lista de opciones terminada

--- src/ConversorApp.java
+++ src/ConversorApp.java
@@ -10,14 +10,82 @@ import java.util.Scanner;
 
 public class ConversorApp {
     public static <JSONObject> void main(String[] args) throws MalformedURLException, RuntimeException {
+
+        int opcion = 0;
+        int cantidad;
+//        cantidad = 0;
+        double USD = 1.5;
 //        int post = 1;
+        System.out.println("********************************");
+
+        String menuOpciones = """
+                1 - Dollar a peso argentino
+                2 - peso argentino a dollar
+                3 - dolar a real brasileño
+                4 - real brasileño a dollar
+                5 - Dollar a peso colombiano
+                6 - Peso colombiano a dollar
+                7 - salir
+                """;
+
+        System.out.println("********************************");
+        System.out.println("Elija una opcion válida");
+        Scanner teclado = new Scanner(System.in);
+
+        while(opcion != 7){
+            System.out.println(menuOpciones);
+            opcion = teclado.nextInt();
+            switch (opcion){
+                case 1:
+                    System.out.println("Dollar a peso argentino");
+                    cantidad = teclado.nextInt();
+                    System.out.println(USD * cantidad + " pesos argentinos");
+                    break;
+
+                case 2:
+                    System.out.println("peso argentino a dollar");
+                    cantidad = teclado.nextInt();
+                    System.out.println(USD / cantidad + " Dolares");
+                    break;
+
+                case 3:
+                    System.out.println("dollar a real brasileño");
+                    cantidad = teclado.nextInt();
+                    System.out.println(USD * cantidad + " Reales brasileños");
+                    break;
+
+                case 4:
+                    System.out.println("Real brasileño a dollar");
+                    cantidad = teclado.nextInt();
+                    System.out.println(USD / cantidad + " Dolares");
+                    break;
+
+                case 5:
+                    System.out.println("dollar a peso comlombiano");
+                    cantidad = teclado.nextInt();
+                    System.out.println(USD * cantidad );
+                    break;
+
+                case 6:
+                    System.out.println("peso colombiano a dollar");
+                    cantidad = teclado.nextInt();
+                    System.out.println(USD / cantidad);
+                    break;
+
+                case 7:
+                    System.out.println("gracias por usar nuestra App");
+                    break;
+                default:
+                    System.out.println("opcion no valida");
+            }
+        }
+
 
         try {
             URL url = new URL("https://v6.exchangerate-api.com/v6/2d8e0c4915085f5e7a6e2bbb/latest/usd");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.connect();
-
             int responseCode = conn.getResponseCode();
             if (responseCode != 200 ){
                 throw new RuntimeException("HttpResponseCode "+ responseCode );
@@ -30,15 +98,13 @@ public class ConversorApp {
                 }
                 scanner.close();
 //                System.out.println(response.toString());
-
                 JsonParser parser = new JsonParser();
                 JsonObject json = parser.parse(response.toString()).getAsJsonObject();
-
                 System.out.println(json.keySet());
             }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 }
